0b8aff6bcc710316360f0682c04f00d5d9ab51c1
Sarah Bader <bader.sarah@icloud.com>
2025-04-10 12:57:12 +0200

    creation of project + added the html files

---
+++ Conferentie/src/main/java/com/springboot/conferentieapp/ConferentieApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.conferentieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferentieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferentieApplication.class, args);
+	}
+
+}
---
+++ Conferentie/src/test/java/com/springboot/conferentieapp/ConferentieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.conferentieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferentieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
