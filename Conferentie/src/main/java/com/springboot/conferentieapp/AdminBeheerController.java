b968a4607853cd8c91e03d6b4f51016723018981
Sarah Bader <bader.sarah@icloud.com>
2025-05-21 14:01:38 +0200

    added management page for admin

---
+++ Conferentie/src/main/java/com/springboot/conferentieapp/AdminBeheerController.java
@@ -0,0 +1,50 @@
+package com.springboot.conferentieapp;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import repository.EvenementRepository;
+import repository.LokaalRepository;
+import repository.SprekerRepository;
+
+@Controller
+@RequiredArgsConstructor
+@Secured("ROLE_ADMIN")
+public class AdminBeheerController {
+
+    private final LokaalRepository lokaalRepo;
+    private final SprekerRepository sprekerRepo;
+    private final EvenementRepository evenementRepo;
+
+    @GetMapping("/beheer")
+    public String showBeheerPagina(Model model) {
+        model.addAttribute("lokalen", lokaalRepo.findAll());
+        model.addAttribute("sprekers", sprekerRepo.findAll());
+        model.addAttribute("evenementen", evenementRepo.findAll());
+        return "beheer";
+    }
+
+    @PostMapping("/lokaal/delete/{id}")
+    public String deleteLokaal(@PathVariable long id) {
+        lokaalRepo.deleteById(id);
+        return "redirect:/admin/beheer";
+    }
+
+    @PostMapping("/spreker/delete/{id}")
+    public String deleteSpreker(@PathVariable long id) {
+        sprekerRepo.deleteById(id);
+        return "redirect:/admin/beheer";
+    }
+
+    @PostMapping("/event/delete/{id}")
+    public String deleteEvent(@PathVariable long id) {
+        evenementRepo.deleteById(id);
+        return "redirect:/admin/beheer";
+    }
+}
